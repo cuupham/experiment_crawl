011ded840f0743cf0d35f0641ac90f91439aa151
cuupham <cuu9991@gmai.com>
2024-03-03 17:33:51 +0700

    upload new file

--- crawl_booktoscrap.py
+++ crawl_booktoscrap.py
@@ -1,16 +1,12 @@
 from selenium import webdriver
-import time
 from selenium.webdriver.common.by import By
 import pandas
 
-"""
-Trang Static
-"""
 
 browser = webdriver.Chrome()
 browser.maximize_window()
 browser.get("https://books.toscrape.com/")
-star_list = ("One","Two","Three","Four","Five")
+star_list = ("One", "Two", "Three", "Four", "Five")
 
 title_tags = browser.find_elements(By.XPATH, "/html/body/div/div/div/div/section/div[2]/ol/li/article/h3/a")
 titles = [
@@ -20,7 +16,7 @@ titles = [
 #     print(tag.get_attribute("title"))
 
 price_tags = browser.find_elements(By.XPATH, "/html/body/div/div/div/div/section/div[2]/ol/li/article/div[2]/p[1]")
-prices  = [tag.text for tag in price_tags]
+prices = [tag.text for tag in price_tags]
 # for tag in price_tags:
 #     print(tag.text)
 
@@ -33,24 +29,13 @@ stars_tags = browser.find_elements(By.XPATH, "/html/body/div/div/div/div/section
 stars = []
 for star in stars_tags:
     star_string = [x for x in star.get_attribute("class").split() if x != "star-rating"][0]
-    stars.append(star_list.index(star_string)+1)
+    stars.append(star_list.index(star_string) + 1)
 
 # Tao dataframe
-df  = pandas.DataFrame({
+df = pandas.DataFrame({
     "title": titles,
     "prices": prices,
     "stars": stars
 })
 
-df.to_excel("books_test.xlsx", index=False)
-
-
-time.sleep(3)
-browser.quit()
-
-
-
-
-
-
-
+df.to_excel("output/books_test.xlsx", index=False)
